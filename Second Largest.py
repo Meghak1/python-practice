333397a1f004e9b36b044dbda5627d482f96b8ef
Meghaa K <140928258+Meghak1@users.noreply.github.com>
2026-05-21 20:27:15 +0530

    https://www.geeksforgeeks.org/problems/second-largest3735/1

---
+++ Second Largest.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getSecondLargest(self, arr):
+        # Code Here
+        lar = sec = -1
+        for num in arr:
+            if num >lar:
+                sec = lar
+                lar = num
+            elif num >sec and num!=lar:
+                sec = num
+        return sec

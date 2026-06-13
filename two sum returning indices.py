1cb21968a9f789063359d725f45e7836d4f59511
Meghaa K <140928258+Meghak1@users.noreply.github.com>
2026-06-13 12:18:10 +0530

    two sum returning indices.py

---
+++ two sum returning indices.py
@@ -0,0 +1,12 @@
+def two_sum(nums, target:int):
+    sum_to_index={}
+    for index, num in enumerate(nums):
+        complement = target-num
+        if complement in sum_to_index:
+            return [sum_to_index[complement],index]
+        sum_to_index[num] = index
+    return []
+nums = [1,4,3,5,9]
+k = 9
+res = two_sum(nums, k)
+print(res)
